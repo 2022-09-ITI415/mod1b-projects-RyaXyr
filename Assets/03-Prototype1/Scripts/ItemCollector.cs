ab6abe890a6cda0b97ae1b5963eeaf376626dcce
RyaXyr <113148836+RyaXyr@users.noreply.github.com>
2022-10-07 04:30:39 -0400

    Building Traps + Movement and Collection Complete
    Char can move, collect, and display collect count. Creating death animations and lives count

--- Assets/03-Prototype1/Scripts/ItemCollector.cs
+++ Assets/03-Prototype1/Scripts/ItemCollector.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
-    private int cherries = 0;
+    private int bananas = 0;
 
-    [SerializeField] private Text cherriesText;
+    [SerializeField] private Text bananasText;
 
-    [SerializeField] private AudioSource collectionSoundEffect;
+  //  [SerializeField] private AudioSource collectionSoundEffect;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Cherry"))
+        if (collision.gameObject.CompareTag("Bananas"))
         {
-            collectionSoundEffect.Play();
+         //   collectionSoundEffect.Play();
             Destroy(collision.gameObject);
-            cherries++;
-            cherriesText.text = "Cherries: " + cherries;
+            bananas++;
+            bananasText.text = "Bananas: " + bananas;
         }
     }
 }
